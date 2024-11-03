a2a0c81af99835b3dc94778605f6034501b59a12
celisej567 <celisej567@gmail.com>
2024-11-03 20:46:32 +0300

    MetaLight namespace fix

--- Assets/metaball/Scripts/MetaLight.cs
+++ Assets/metaball/Scripts/MetaLight.cs
@@ -1,20 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(Light))]
-public class MetaLight : MonoBehaviour {
-    public MetaBall linkedMetaball;
-    public float intensity;
+namespace MetaBalls
+{
+    [RequireComponent(typeof(Light))]
+    public class MetaLight : MonoBehaviour
+    {
+        public MetaBall linkedMetaball;
+        public float intensity;
 
-    private float phaseShift;
-    private Light lightComponent;
+        private float phaseShift;
+        private Light lightComponent;
 
-    void Start () {
-        this.phaseShift = Random.Range(0, Mathf.PI);
-        this.lightComponent = this.GetComponent<Light>();
+        void Start()
+        {
+            this.phaseShift = Random.Range(0, Mathf.PI);
+            this.lightComponent = this.GetComponent<Light>();
+        }
+
+        void Update()
+        {
+            this.lightComponent.intensity = this.intensity * (this.linkedMetaball.transform.localPosition.magnitude + (1 + Mathf.Sin(this.phaseShift + Time.time / 2)));
+        }
     }
-
-    void Update () {
-        this.lightComponent.intensity = this.intensity * (this.linkedMetaball.transform.localPosition.magnitude + (1 + Mathf.Sin(this.phaseShift + Time.time/2)));
-	}
 }
